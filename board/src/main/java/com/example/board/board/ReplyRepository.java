c305ad2e8b58a9a1a38088eaa87f79da316c3109
grain02 <WorkSY1111@gmail.com>
2021-09-10 21:13:44 +0900

    전체 파일 업데이트

--- board/src/main/java/com/example/board/board/ReplyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.board.board;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface ReplyRepository extends JpaRepository<Reply,Integer> {
-   List<Reply> findAllBySeqBoard(int seqBoard);
-}
