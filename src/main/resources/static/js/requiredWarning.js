9ae19faee21b2cf3918286095665cb09ca09d23a
bellasimi <WorkSY1111@gmail.com>
2021-10-09 23:57:06 +0900

    [Back] null값 warning js 수정

--- src/main/resources/static/js/requiredWarning.js
+++ src/main/resources/static/js/requiredWarning.js
@@ -1,33 +1,39 @@
 const yesBtn = document.querySelector('.btn-yes');
 
-const id = document.querySelector('#id');
+/*const id = document.querySelector('#id');
 const pw = document.querySelector('#pw');
 
 const yourId = document.querySelector('#yourId');
 const yourPw = document.querySelector('#yourId');
 const name = document.querySelector('#yourName');
-const tel = document.querySelector('#yourTel');
+const tel = document.querySelector('#yourTel');*/
 
+const id = document.getElementById('id');
+const pw = document.getElementById('pw');
+
+const yourId = document.getElementById('yourId');
+const yourPw = document.getElementById('yourId');
+const name = document.getElementById('yourName');
+const tel = document.getElementById('yourTel');
 
 yesBtn.addEventListener('click',() => {
 
-    console.log("3"+id.valueOf());
-    if(id.val() == ''){
+  if(id.value == null){
         id.nextElementSibling.classList.toggle('warning');
     }
-    else if(pw == ''){
+    else if(pw.value == null){
         pw.nextElementSibling.classList.toggle('warning');
     }
-    else if(yourId == ''){
+    else if(yourId.value == null){
         yourId.nextElementSibling.classList.toggle('warning');
     }
-    else if(yourPw == ''){
+    else if(yourPw.value == null){
         yourPw.nextElementSibling.classList.toggle('warning');
     }
-    else if(name == ''){
+    else if(name.value == null){
         name.nextElementSibling.classList.toggle('warning');
     }
-    else if(tel == ''){
+    else if(tel.value == null){
         tel.nextElementSibling.classList.toggle('warning');
     }
 
