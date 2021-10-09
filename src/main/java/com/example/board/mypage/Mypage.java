64c269a324f8b21ce589ba88c9f357427d98e586
bellasimi <WorkSY1111@gmail.com>
2021-10-10 00:40:20 +0900

    [Back] mypage 개체 추가

---
+++ src/main/java/com/example/board/mypage/Mypage.java
@@ -0,0 +1,28 @@
+package com.example.board.mypage;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Mypage {
+
+    @Id
+    @GeneratedValue
+    private int mypageSeq;
+    @Column(nullable = false)
+    private String seqMember;
+    @Column(nullable = false)
+    private String seqBoard;
+
+}
