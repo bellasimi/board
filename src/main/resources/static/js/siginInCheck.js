1d29b0f7127296365a05344203c99994187cd7c4
bellasimi <WorkSY1111@gmail.com>
2021-10-15 20:55:01 +0900

    [Back] 회원가입시 유효성검사

--- src/main/resources/static/js/siginInCheck.js
+++ src/main/resources/static/js/siginInCheck.js
@@ -1,9 +1,11 @@
 const yesBtn = document.querySelector('.btn-yes');
+const form = document.getElementById('memberForm');
 
 const yourId = document.getElementById('yourId');
-const yourPw = document.getElementById('yourId');
+const yourPw = document.getElementById('yourPw');
+const yourName = document.getElementById('yourName');
 const yourEmail = document.getElementById('yourEmail');
-const tel = document.getElementById('yourTel');
+const yourTel = document.getElementById('yourTel');
 
 const idReg = /^[a-zA-Z0-9]{4,12}$/;
 const pwReg = /\d{4,8}/;
@@ -13,19 +15,29 @@ const telReg =/^d{9,11}/;
 yesBtn.addEventListener('click',()=>{
     if(!idReg.test(yourId.value)){
         alert("id는 영문숫자혼합 4-12자로 입력해주세요!");
+        yourId.select();
         yourId.focus();
+        return false;
     }
-    else if(pwReg.test(yourPw.value)){
+    if(!pwReg.test(yourPw.value)){
         alert("비밀번호는 숫자 4-8자로 입력해주세요!");
+        console.log(yourPw.value)
+        yourPw.select();
         yourPw.focus();
+        return false;
     }
-    else if(emailReg.test(yourEmail.value)){
+    if(!emailReg.test(yourEmail.value)){
         alert("이메일은 www@www.www형식으로 입력해주세요!");
+        yourEmail.select();
         yourEmail.focus();
+        return false;
     }
-    else if(telReg.test(tel.value)){
+    if(!telReg.test(yourTel.value)){
         alert("전화번호는 -를 빼고 입력해주세요!");
-        tel.focus();
+        yourTel.select();
+        yourTel.focus();
+        return false;
     }
 
+        form.submit();
 });
